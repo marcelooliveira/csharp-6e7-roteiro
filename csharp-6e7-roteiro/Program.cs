89befe73dd2464e1e4495145e743634311734537
marcelooliveira <mclricardo@gmail.com>
2018-01-04 15:19:57 -0200

    .

---
+++ csharp-6e7-roteiro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_6e7_roteiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
