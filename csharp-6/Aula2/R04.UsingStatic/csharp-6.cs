c20fda37cce21d4d8eebce398c948aaf2aa3b3e4
marcelooliveira <mclricardo@gmail.com>
2018-01-12 12:47:29 -0200

    .

--- csharp-6/Aula2/R04.UsingStatic/csharp-6.cs
+++ csharp-6/Aula2/R04.UsingStatic/csharp-6.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.String;
+using static System.DateTime;
+using static CSharp6.R04.Ano;
 
 namespace CSharp6.R04
 {
@@ -24,6 +26,152 @@ namespace CSharp6.R04
         }
     }
 
+    //1. Vamos adicionar as propriedades Endereco e Telefone, além da propriedade calculada DadosPessoais
+    //class Aluno
+    //{
+    //    public string Nome { get; }
+    //    public string Sobrenome { get; }
+    //    public string Endereco { get; set; }
+    //    public string Telefone { get; set; }
+
+    //    public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
+
+    //    public Aluno(string nome, string sobrenome)
+    //    {
+    //        this.Nome = nome;
+    //        this.Sobrenome = sobrenome;
+    //    }
+
+    //    public Aluno(string nome, string sobrenome, DateTime dataNascimento) : this(nome, sobrenome)
+    //    {
+    //        this.DataNascimento = dataNascimento;
+    //    }
+
+    //    public string NomeCompleto => string.Format("{0} {1}", Nome, Sobrenome);
+
+    //    public int GetIdade()
+    //        => (int)((DateTime.Now - DataNascimento).TotalDays / 365.242199);
+
+    //    public string DadosPessoais =>
+    //        string.Format("Nome: {0}, " +
+    //            "Endereço: {1}, " +
+    //            "Telefone: {2}", NomeCompleto, Endereco, Telefone);
+    //}
+
+
+
+
+    //2. vamos usar using static String e using static DateTime
+    //class Aluno
+    //{
+    //    public string Nome { get; }
+    //    public string Sobrenome { get; }
+    //    public string Endereco { get; set; }
+    //    public string Telefone { get; set; }
+
+    //    public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
+
+    //    public Aluno(string nome, string sobrenome)
+    //    {
+    //        this.Nome = nome;
+    //        this.Sobrenome = sobrenome;
+    //    }
+
+    //    public Aluno(string nome, string sobrenome, DateTime dataNascimento) : this(nome, sobrenome)
+    //    {
+    //        this.DataNascimento = dataNascimento;
+    //    }
+
+    //    public string NomeCompleto => Format("{0} {1}", Nome, Sobrenome);
+
+    //    public int GetIdade()
+    //        => (int)((Now - DataNascimento).TotalDays / 365.242199);
+
+    //    public string DadosPessoais =>
+    //        Format("Nome: {0}, " +
+    //            "Endereço: {1}, " +
+    //            "Telefone: {2}", NomeCompleto, Endereco, Telefone);
+    //}
+
+
+
+
+    //3. Vamos criar uma propriedade AnoNaEscola, que armazenará
+    //um valor da enumeracao AnoEscolar. Também vamos criar a propriedade AnoNaEscola
+    //e o método PontosDeExperiencia():
+    //public enum Ano
+    //{
+    //    Primeiro,
+    //    Segundo,
+    //    Terceiro,
+    //    Quarto
+    //}
+
+    //class Aluno
+    //{
+    //    public string Nome { get; }
+    //    public string Sobrenome { get; }
+    //    public string Endereco { get; set; }
+    //    public string Telefone { get; set; }
+
+    //    public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
+
+    //    public Ano AnoNaEscola { get; set; } = Ano.Primeiro;
+
+    //    public int PontosDeExperiencia()
+    //    {
+    //        switch (AnoNaEscola)
+    //        {
+    //            case Ano.Primeiro:
+    //                return 0;
+    //            case Ano.Segundo:
+    //                return 15;
+    //            case Ano.Terceiro:
+    //                return 65;
+    //            case Ano.Quarto:
+    //                return 80;
+    //            default:
+    //                return 0;
+    //        }
+    //    }
+
+    //    public Aluno(string nome, string sobrenome)
+    //    {
+    //        this.Nome = nome;
+    //        this.Sobrenome = sobrenome;
+    //    }
+
+    //    public Aluno(string nome, string sobrenome, DateTime dataNascimento) : this(nome, sobrenome)
+    //    {
+    //        this.DataNascimento = dataNascimento;
+    //    }
+
+    //    public string NomeCompleto => Format("{0} {1}", Nome, Sobrenome);
+
+    //    public int GetIdade()
+    //        => (int)((Now - DataNascimento).TotalDays / 365.242199);
+
+    //    public string DadosPessoais =>
+    //        Format("Nome: {0}, " +
+    //            "Endereço: {1}, " +
+    //            "Telefone: {2}", NomeCompleto, Endereco, Telefone);
+    //}
+
+
+
+
+
+
+    //4. Agora podemos acessar diretamente os membros da enumeração
+    //com using static Ano:
+    public enum Ano
+    {
+        Primeiro,
+        Segundo,
+        Terceiro,
+        Quarto
+    }
+
     class Aluno
     {
         public string Nome { get; }
@@ -32,6 +180,25 @@ namespace CSharp6.R04
         public string Telefone { get; set; }
 
         public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
+
+        public Ano AnoNaEscola { get; set; } = Primeiro;
+
+        public int PontosDeExperiencia()
+        {
+            switch (AnoNaEscola)
+            {
+                case Primeiro:
+                    return 0;
+                case Segundo:
+                    return 15;
+                case Terceiro:
+                    return 65;
+                case Quarto:
+                    return 80;
+                default:
+                    return 0;
+            }
+        }
 
         public Aluno(string nome, string sobrenome)
         {
@@ -44,14 +211,15 @@ namespace CSharp6.R04
             this.DataNascimento = dataNascimento;
         }
 
-        public string NomeCompleto => string.Format("{0} {1}", Nome, Sobrenome);
+        public string NomeCompleto => Format("{0} {1}", Nome, Sobrenome);
 
         public int GetIdade()
-            => (int)((DateTime.Now - DataNascimento).TotalDays / 365.242199);
+            => (int)((Now - DataNascimento).TotalDays / 365.242199);
 
         public string DadosPessoais =>
-            string.Format("Nome: {0}, " +
+            Format("Nome: {0}, " +
                 "Endereço: {1}, " +
                 "Telefone: {2}", NomeCompleto, Endereco, Telefone);
     }
+
 }
