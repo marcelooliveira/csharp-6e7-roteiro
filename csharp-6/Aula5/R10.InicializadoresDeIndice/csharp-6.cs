c93b15c4e415be6b5f396137cd33cf764d9ab1d1
marcelooliveira <mclricardo@gmail.com>
2018-01-15 12:31:32 -0200

    .

--- csharp-6/Aula5/R10.InicializadoresDeIndice/csharp-6.cs
+++ csharp-6/Aula5/R10.InicializadoresDeIndice/csharp-6.cs
@@ -1,58 +1,90 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using static System.String;
+using static System.DateTime;
+using static CSharp6.R10.Ano;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.IO;
 
 namespace CSharp6.R10
 {
+    //1. C# 6 agora permite inicializar índices, como em dicionários
     class Programa
     {
-        public void Main()
+        public async void Main()
         {
             Console.WriteLine("10. Inicializadores De Índice");
+
+            StreamWriter logger = new StreamWriter("LogDoCurso.txt");
+            try
+            {
+                Aluno marty = new Aluno("Marty", "McFly", new DateTime(1968, 06, 12))
+                {
+                    Endereco = "9303 Lyon Drive Hill Valley CA",
+                    Telefone = "555-4385"
+                };
+
+                await logger.WriteLineAsync("Aluno Marty McFly criado");
+
+                Console.WriteLine(marty.Nome);
+                Console.WriteLine(marty.Sobrenome);
+                Console.WriteLine(marty.DadosPessoais);
+                Avaliacao melhorAvaliacao = GetMelhorNota(marty);
+
+                Console.WriteLine("Melhor Nota: {0}", melhorAvaliacao?.Nota);
+
+                marty.AdicionarAvaliacao(new Avaliacao(1, "GEO", 8));
+                marty.AdicionarAvaliacao(new Avaliacao(1, "MAT", 6));
+                marty.AdicionarAvaliacao(new Avaliacao(1, "HIS", 7));
+
+                melhorAvaliacao = GetMelhorNota(marty);
+
+                Console.WriteLine("Melhor Nota: {0}", melhorAvaliacao?.Nota);
+
+                foreach (var avaliacao in marty.Avaliacoes)
+                {
+                    Console.WriteLine(avaliacao);
+                }
+
+                marty.PropertyChanged += (sender, eventArgs) =>
+                {
+                    Console.WriteLine($"Propriedade {eventArgs.PropertyName} mudou!");
+                };
+                marty.Endereco = "novo endereço";
+                marty.Telefone = "7777777";
+
+                Aluno biff = new Aluno("Biff", "");
+
+            }
+            catch (ArgumentException exc) when (exc.Message.Contains("não informado"))
+            {
+                Console.WriteLine($"ERRO: O parâmetro '{exc.ParamName}' não foi informado!");
+                await logger.WriteLineAsync("Erro: " + exc.ToString());
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+                await logger.WriteLineAsync(exc.ToString());
+            }
+            finally
+            {
+                await logger.WriteLineAsync("O programa terminou.");
+                logger.Dispose();
+            }
+        }
+
+        private static Avaliacao GetMelhorNota(Aluno marty)
+        {
+            return marty.Avaliacoes
+                .OrderByDescending(a => a.Nota)
+                .FirstOrDefault();
         }
     }
 
-    class CSharp6
-    {
-        private List<string> messages = new List<string>
-        {
-            "Page not Found",
-            "Page moved, but left a forwarding address.",
-            "The web server can't come out to play today."
-        };
 
-        private Dictionary<int, string> webErrors = new Dictionary<int, string>
-        {
-            [404] = "Page not Found",
-            [302] = "Page moved, but left a forwarding address.",
-            [500] = "The web server can't come out to play today."
-        };
-
-    }
-
-    public class ListaDeMatricula : IEnumerable<Aluno>
-    {
-        private List<Aluno> todosAlunos = new List<Aluno>();
-
-        public void Matricular(Aluno s)
-        {
-            todosAlunos.Add(s);
-        }
-
-        public IEnumerator<Aluno> GetEnumerator()
-        {
-            return ((IEnumerable<Aluno>)todosAlunos).GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return ((IEnumerable<Aluno>)todosAlunos).GetEnumerator();
-        }
-    }
 
     public enum Ano
     {
@@ -62,53 +94,179 @@ namespace CSharp6.R10
         Quarto
     }
 
-    public class Aluno
+    class Aluno : INotifyPropertyChanged
     {
-        public string Prenome { get; }
+        public string Nome { get; }
         public string Sobrenome { get; }
 
-        public ICollection<double> Notas { get; } = new List<double>();
-        public Ano AnoNaEscola { get; set; } = Ano.Primeiro;
-
-        public string NomeCompleto => $"{Prenome} {Sobrenome}";
-
-
-        public Aluno(string prenome, string sobrenome)
+        private string endereco;
+        public string Endereco
         {
-            if (IsNullOrWhiteSpace(sobrenome))
-                throw new ArgumentException(message: "Não pode ser vazio", paramName: nameof(sobrenome));
-
-            Prenome = prenome;
-            Sobrenome = sobrenome;
+            get { return endereco; }
+            set
+            {
+                if (endereco != value)
+                {
+                    endereco = value;
+                    PropertyChanged.Call(this);
+                    PropertyChanged.Call(this, nameof(DadosPessoais));
+                }
+            }
         }
 
-        public void MudarNome(string novoSobrenome)
+        private string telefone;
+        public string Telefone
         {
-            // Produz erro: CS0200: Property or indexer cannot be assigned to -- it is read only
-            //Sobrenome = novoSobrenome;
+            get { return telefone; }
+            set
+            {
+                if (endereco != value)
+                {
+                    telefone = value;
+                    PropertyChanged.Call(this);
+                    PropertyChanged.Call(this, nameof(DadosPessoais));
+                }
+            }
         }
 
-        public override string ToString() => $"{Sobrenome}, {Prenome}";
+        public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
 
-        public string GetNotaMedia() =>
-            $"Name: {Sobrenome}, {Prenome}. G.P.A: {Notas.Average()}";
+        public Ano AnoNaEscola { get; set; } = Primeiro;
 
-        public string GetPorcentagemNotaMedia() =>
-            $"Name: {Sobrenome}, {Prenome}. G.P.A: {Notas.Average():F2}";
-
-        public string GetPorcentagemNotaMedias() =>
-            $"Name: {Sobrenome}, {Prenome}. G.P.A: {(Notas.Any() ? Notas.Average() : double.NaN):F2}";
-
-        public string GetTodasNotas() =>
-            $@"All Notas: {Notas.OrderByDescending(g => g)
-            .Select(s => s.ToString("F2")).Aggregate((parcial, elemento) => $"{parcial}, {elemento}")}";
-
-        public bool EntrouNaListaDeHonra()
+        public int PontosDeExperiencia()
         {
-            return Notas.All(g => g > 3.5) && Notas.Any();
-            // Code below generates CS0103: 
-            // The name 'All' does not exist in the current context.
-            //All(Notas, g => g > 3.5) && Notas.Any();
+            switch (AnoNaEscola)
+            {
+                case Primeiro:
+                    return 0;
+                case Segundo:
+                    return 15;
+                case Terceiro:
+                    return 65;
+                case Quarto:
+                    return 80;
+                default:
+                    return 0;
+            }
+        }
+
+        public Aluno(string nome, string sobrenome)
+        {
+            VerificarParametro(nome, nameof(nome));
+            VerificarParametro(sobrenome, nameof(sobrenome));
+
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+        }
+
+        private static void VerificarParametro(string valorDoParametro, string nomeDoParametro)
+        {
+            if (string.IsNullOrEmpty(valorDoParametro))
+            {
+                throw new ArgumentException("Parâmetro não informado", nomeDoParametro);
+            }
+        }
+
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento) : this(nome, sobrenome)
+        {
+            this.DataNascimento = dataNascimento;
+        }
+
+        public string NomeCompleto => $"{Nome} {Sobrenome}";
+
+        public int GetIdade()
+            => (int)((Now - DataNascimento).TotalDays / 365.242199);
+
+        public string DadosPessoais =>
+            $"Nome: {NomeCompleto}, " +
+            $"Nascimento: {DataNascimento:dd/MM/yyyy}, " +
+            $"Endereço: {Endereco}, " +
+            $"Telefone: {Telefone}";
+
+        private IList<Avaliacao> avaliacoes = new List<Avaliacao>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IReadOnlyCollection<Avaliacao> Avaliacoes
+            => new ReadOnlyCollection<Avaliacao>(avaliacoes);
+
+        public void AdicionarAvaliacao(Avaliacao avaliacao)
+        {
+            avaliacoes.Add(avaliacao);
+        }
+
+    }
+
+    static class PropertyChangedExtensions
+    {
+        public static void Call(this PropertyChangedEventHandler handler,
+            object sender, [CallerMemberName] string propertyName = null)
+        {
+            if (handler != null)
+            {
+                handler.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    //1. Vamos criar um dicionário de matérias e popular esse dicionário
+    //class Avaliacao
+    //{
+    //    Dictionary<string, string> materias = new Dictionary<string, string>();
+
+    //    public Avaliacao(int bimestre, string materia, double nota)
+    //    {
+    //        Bimestre = bimestre;
+    //        Materia = materia;
+    //        Nota = nota;
+    //        materias.Add("MAT", "Matemática");
+    //        materias.Add("LPL", "Língua Portuguesa");
+    //        materias.Add("FIS", "Física");
+    //        materias.Add("HIS", "História");
+    //        materias.Add("GEO", "Geografia");
+    //        materias.Add("QUI", "Química");
+    //        materias.Add("BIO", "Biologia");
+    //    }
+
+    //    public int Bimestre { get; }
+    //    public string Materia { get; }
+    //    public double Nota { get; }
+
+    //    public override string ToString()
+    //    {
+    //        return $"Bimestre: {Bimestre} - Matéria: {materias[Materia]} - Nota: {Nota}";
+    //    }
+    //}
+
+
+    //2. C# 6 permite inicializar índices, como em dicionários
+    class Avaliacao
+    {
+        Dictionary<string, string> materias = new Dictionary<string, string>
+        {
+            ["MAT"] = "Matemática",
+            ["LPL"] = "Língua Portuguesa",
+            ["FIS"] = "Física",
+            ["HIS"] = "História",
+            ["GEO"] = "Geografia",
+            ["QUI"] = "Química",
+            ["BIO"] = "Biologia"
+        };
+
+        public Avaliacao(int bimestre, string materia, double nota)
+        {
+            Bimestre = bimestre;
+            Materia = materia;
+            Nota = nota;
+        }
+
+        public int Bimestre { get; }
+        public string Materia { get; }
+        public double Nota { get; }
+
+        public override string ToString()
+        {
+            return $"Bimestre: {Bimestre} - Matéria: {materias[Materia]} - Nota: {Nota}";
         }
     }
 }
