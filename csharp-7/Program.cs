3d03bdfa948690a3734916a4186eca0e84dbdebe
marcelooliveira <mclricardo@gmail.com>
2018-01-24 11:29:28 -0200

    .

--- csharp-7/Program.cs
+++ csharp-7/Program.cs
@@ -10,6 +10,49 @@ namespace CSharp7
     {
         static void Main(string[] args)
         {
+            string[] menus = new string[] {
+                "1. Variáveis out"
+            };
+
+            Console.WriteLine("ÍNDICE DE PROGRAMAS");
+            Console.WriteLine("===================");
+
+
+            string line;
+            do
+            {
+                foreach (var menu in menus)
+                {
+                    Console.WriteLine(menu);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Escolha um programa:");
+
+                line = Console.ReadLine();
+
+
+                Int32.TryParse(line, out int programa);
+
+                Console.WriteLine();
+                Console.WriteLine(menus[programa - 1]);
+                Console.WriteLine();
+
+                switch (programa)
+                {
+                    case 1:
+                        new csharp7.R01.antes.Programa().Main();
+                        new csharp7.R01.depois.Programa().Main();
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PRESSIONE UMA TECLA PARA CONTINUAR...");
+                Console.ReadKey();
+                Console.Clear();
+            } while (line.Length > 0);
         }
     }
 }
