eb2c94123c87e39ba3f2a315550bee39f6f3a337
marcelooliveira <mclricardo@gmail.com>
2018-01-24 15:56:11 -0200

    .

---
+++ csharp-7/R04.CorrespondenciaDePadroes/antes/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace csharp7.R04.antes
+{
+    class MenuItem : csharp7.MenuItem
+    {
+        public override void Main()
+        {
+            var entradas = new Stack<object>();
+            entradas.Push(2);
+            entradas.Push("+");
+            entradas.Push(3);
+
+            var calculadora = new Calculadora();
+            foreach (var entrada in entradas)
+            {
+                calculadora.Digitar(entradas);
+            }
+
+            double valor = calculadora.Valor;
+            Console.WriteLine(valor);
+        }
+    }
+
+    internal class Calculadora
+    {
+        public Calculadora()
+        {
+        }
+
+        public double Valor { get; internal set; }
+        public double Display { get; internal set; }
+        public object UltimaEntrada { get; internal set; }
+
+        internal void Digitar(object entrada)
+        {
+            switch (entrada)
+            {
+                case int valor:
+                    break;
+                case "+":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ csharp-7/R04.CorrespondenciaDePadroes/depois/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace csharp7.R04.depois
+{
+    class MenuItem : csharp7.MenuItem
+    {
+        public override void Main()
+        {
+        }
+    }
+}
